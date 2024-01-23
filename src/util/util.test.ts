11162ffea287cfefee9ca9959c0dc1c4d433855e
Dongsheng Xi <dongsheng.xi@oculus.com>
2024-01-23 17:26:57 +0800

    setup lint

--- src/util/util.test.ts
+++ src/util/util.test.ts
@@ -1,12 +1,11 @@
 import { groupByUsers, groupByUsersFunction } from './util';
 
-test('test group by user', () => {
-  let group_result = { '4': [ 'nine', 'four' ], '5': [ 'eight', 'seven' ] };
+test('group by user', () => {
+  let group_result = { '4': ['nine', 'four'], '5': ['eight', 'seven'] };
   expect(groupByUsers()).toEqual(group_result);
 });
 
-
-test('test group by user function', () => {
-  let group_result = { '4': [ 'nine', 'four' ], '5': [ 'eight', 'seven' ] };
+test('group by user function', () => {
+  let group_result = { '4': ['nine', 'four'], '5': ['eight', 'seven'] };
   expect(groupByUsersFunction()).toEqual(group_result);
 });
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,23 +1,21 @@
-import { groupBy } from "lodash";
+import { groupBy } from 'lodash';
 
+export const groupByUsers = (): Object => {
+  // Original array
 
-export const groupByUsers = () : Object => {
-    // Original array 
+  let users = ['eight', 'nine', 'four', 'seven'];
+  let grouped_data = groupBy(users, 'length');
 
-    let users = (['eight', 'nine', 'four', 'seven']);
-    let grouped_data = groupBy(users, 'length')
-    
-    console.log(grouped_data);
-    return grouped_data;
+  console.log(grouped_data);
+  return grouped_data;
+};
+
+export function groupByUsersFunction(): Object {
+  // Original array
+
+  let users = ['eight', 'nine', 'four', 'seven'];
+  let grouped_data = groupBy(users, 'length');
+
+  console.log(grouped_data);
+  return grouped_data;
 }
-
-export function groupByUsersFunction() : Object {
-    // Original array 
-
-    let users = (['eight', 'nine', 'four', 'seven']);
-    let grouped_data = groupBy(users, 'length')
-    
-    console.log(grouped_data);
-    return grouped_data;
-}
-
